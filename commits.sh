0326068968ed861d2c86edfbbc2647e211594b45
Taylor Troesh <surprisetalk@gmail.com>
2024-01-24 08:22:50 -0800

    ℹ

--- commits.sh
+++ commits.sh
@@ -2,32 +2,36 @@
 
 GITHUB_TOKEN="$1"
 
-while read REPO; do
+while : ; do
 
-  sleep 10
+  shuf repos.txt | while read REPO; do
+  
+    sleep 10
+  
+    LICENSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+                      -H "Accept: application/vnd.github.v3+json" \
+                         "https://api.github.com/repos/$REPO/license" | jq -r '.path')
+  
+    if [ ! -z "$LICENSE" -a "$LICENSE" != "null" ]; then
+  
+      COMMIT_DATA=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+                            -H "Accept: application/vnd.github.v3+json" \
+                               "https://api.github.com/repos/$REPO/commits?path=$LICENSE")
+      
+      echo "$COMMIT_DATA" | jq -c '.[]' | while IFS= read -r line; do
+  
+          COMMITED_AT=$(echo "$line" | jq -r '.commit.author.date')
+          COMMIT_SHA=$(echo "$line" | jq -r '.sha')
+      
+          ROW="$COMMITED_AT,$REPO,$COMMIT_SHA"
+          
+          # Check if the row exists in commits.csv, and if not, append it
+          grep -qxF "$ROW" commits.csv || echo "$ROW" >> commits.csv
+  
+      done
+  
+    fi
+  
+  done
 
-  LICENSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
-                    -H "Accept: application/vnd.github.v3+json" \
-                       "https://api.github.com/repos/$REPO/license" | jq -r '.path')
-
-  if [ ! -z "$LICENSE" -a "$LICENSE" != "null" ]; then
-
-    COMMIT_DATA=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
-                          -H "Accept: application/vnd.github.v3+json" \
-                             "https://api.github.com/repos/$REPO/commits?path=$LICENSE")
-    
-    echo "$COMMIT_DATA" | jq -c '.[]' | while IFS= read -r line; do
-
-        COMMITED_AT=$(echo "$line" | jq -r '.commit.author.date')
-        COMMIT_SHA=$(echo "$line" | jq -r '.sha')
-    
-        ROW="$COMMITED_AT,$REPO,$COMMIT_SHA"
-        
-        # Check if the row exists in commits.csv, and if not, append it
-        grep -qxF "$ROW" commits.csv || echo "$ROW" >> commits.csv
-
-    done
-
-  fi
-
-done < repos.txt
+done
--- repos.sh
+++ repos.sh
@@ -5,17 +5,26 @@ GITHUB_TOKEN="$1"
 PAGE=1
 
 while : ; do
-    RESPONSE=$(curl -H "Authorization: token $GITHUB_TOKEN" \
-                    -H "Accept: application/vnd.github.v3+json" \
-                      "https://api.github.com/search/repositories?sort=stars&order=desc&per_page=100&page=$PAGE")
+    RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+                       -H "Accept: application/vnd.github.v3+json" \
+                          "https://api.github.com/search/repositories?q=stars:>1000&sort=updated&order=desc&per_page=100&page=$PAGE")
     
     COUNT=$(echo "$RESPONSE" | jq '.items | length')
     if [[ "$COUNT" -eq 0 ]]; then
-        break
+      PAGE=1
     fi
-    
-    echo "$RESPONSE" | jq -r '.items[] | "\(.full_name)"' >> repos.txt
-    echo "$RESPONSE" | jq -r '.items[] | "\(.full_name),\(.license.spdx_id)"' >> licenses.csv
+
+    echo "$RESPONSE" | jq -r '.items[] | .full_name' | while read REPO; do
+      if ! grep -qxF "$REPO" repos.txt; then
+        echo "$REPO" >> repos.txt
+      fi
+    done
+
+    echo "$RESPONSE" | jq -r '.items[] | "\(.full_name),\(.license.spdx_id)"' | while read LICENSE; do
+      if ! grep -qxF "$LICENSE" licenses.csv; then
+        echo "$LICENSE" >> licenses.csv
+      fi
+    done
 
     ((PAGE++))
 done
